488ed177bf495aba2c0cdddc458884ce7be821f0
Przemyslaw Pokrywka <przemyslaw.pokrywka@gmail.com>
2010-11-15 22:01:55 +0100

    GateActor starts to look nice

--- src/main/scala/wrasse3/GateActor.scala
+++ src/main/scala/wrasse3/GateActor.scala
@@ -6,12 +6,11 @@ import actors.{Actor, TIMEOUT}
 
 class GateActor(srv: Service, CLOSE_PERIOD: Int = 500) extends Actor {
 
-  var errors = 0
-  var end: Long = 0
 
   def act = loop{
 
     println("entering open state")
+    var errors = 0
 
     loopWhile(errors < 10) {
       react{
@@ -27,8 +26,7 @@ class GateActor(srv: Service, CLOSE_PERIOD: Int = 500) extends Actor {
     } andThen {
 
       println("entering closed state")
-      errors = 0
-      end = now + CLOSE_PERIOD
+      val end = now + CLOSE_PERIOD
 
       loopWhile(now < end) {
         reactWithin(end - now) {
