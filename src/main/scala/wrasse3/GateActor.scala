142e9603f94bb8021d588c9ecf59ddbfcb89e475
Przemyslaw Pokrywka <przemyslaw.pokrywka@gmail.com>
2010-11-15 21:50:56 +0100

    small readability fixes

--- src/main/scala/wrasse3/GateActor.scala
+++ src/main/scala/wrasse3/GateActor.scala
@@ -4,32 +4,22 @@ import scala.actors.Actor._
 import System.{currentTimeMillis => now}
 import actors.{Actor, TIMEOUT}
 
-/**
- * Created by IntelliJ IDEA.
- * User: przemek
- * Date: 09.11.10
- * Time: 18:02
- * To change this template use File | Settings | File Templates.
- */
-
 class GateActor(srv: Service, CLOSE_PERIOD: Int = 500) extends Actor {
 
-  var subsequentErrors = 0
+  var errors = 0
   var end: Long = 0
-
-  def tenErrorsARow = subsequentErrors >= 10
 
   def act = loop{
 
     println("entering open state")
 
-    loopWhile(!tenErrorsARow) {
+    loopWhile(errors < 10) {
       react{
         case _ => {
           val r = srv.hit()
           r match {
-            case ErrorResponse => subsequentErrors += 1
-            case _ => subsequentErrors = 0
+            case ErrorResponse => errors += 1
+            case _ => errors = 0
           }
           reply(r)
         }
@@ -37,7 +27,7 @@ class GateActor(srv: Service, CLOSE_PERIOD: Int = 500) extends Actor {
     } andThen {
 
       println("entering closed state")
-      subsequentErrors = 0
+      errors = 0
       end = now + CLOSE_PERIOD
 
       loopWhile(now < end) {
